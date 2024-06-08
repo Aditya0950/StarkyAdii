9095fd32a081682652240b105531c819b6e073cb
Starky Adii <121501207+Aditya0950@users.noreply.github.com>
2024-06-08 20:48:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
-const logo = document.querySelector('#leftSectionBox');
-const logoText = document.querySelector('#leftSection a h1');
-
 function logoAnime() {
+  const logo = document.querySelector('#leftLogo');
   let tl = gsap.timeline();
 
   logo.addEventListener("mouseenter", function () {
-    tl.to(logoText, {
+    tl.to('#navLeft p', {
       opacity: 1
     })
   })
 
   logo.addEventListener("mouseleave", function () {
-    tl.to(logoText, {
+    tl.to('#navLeft p', {
       opacity: 0
     })
   })
@@ -27,37 +25,36 @@ function navAnime() {
     duration: 1
   })
 
-  tl.from('#navContainer #leftSection', {
+  tl.from('nav #navLeft', {
     x: -1500,
     duration: 0.75
   })
 
-  tl.from('#leftSection #leftSectionBox', {
+  tl.from('#navLeft #leftLogo', {
     x: -1500,
     duration: 0.75
   })
 
-  tl.from('#navContainer .navSection', {
+  tl.from('#navRight .rightBox', {
     y: -100,
     opacity: 0,
     duration: 0.85,
     stagger: 0.25
   })
 
-  tl.from('#navContainer .navSection', {
+  tl.from('#navRight .rightBox', {
     duration: 0.85,
     stagger: 0.25
   })
 }
 
-navAnime();
-
-const crsr = document.querySelector('#container #cursor');
-const crsrDot = document.querySelector('#cursor #cursorDot');
-const container = document.querySelector('#container');
-const nav = document.querySelector('nav');
+// navAnime();
 
 function cursor() {
+  const crsr = document.querySelector('#container #cursor');
+  const crsrDot = document.querySelector('#cursor #cursorDot');
+  const container = document.querySelector('#container');
+
   container.addEventListener("mousemove", function (dets) {
     crsr.style.left = dets.pageX + "px";
     crsr.style.top = dets.pageY + "px";
@@ -72,147 +69,68 @@ function cursor() {
     crsrDot.style.left = dets.pageX + "px";
     crsrDot.style.top = dets.pageY + "px";
   })
-
-  nav.addEventListener('mouseenter', () => {
-    crsr.style.zIndex = '101';
-  })
-
-  nav.addEventListener('mouseleave', () => {
-    crsr.style.zIndex = '8';
-  })
 }
 
 cursor();
 
-const mainContainerText1 = document.querySelector('#mainContainer #page1 h1');
-const mainPage2 = document.querySelector('#mainContainer #page2Container');
-const mainPage2Text = document.querySelector('#mainContainer #page2 #page2Container h1');
-const mainPage2Text2 = document.querySelector('#mainContainer #page2 #page2Container h2');
-
-function mainTextGsap() {
+function secondPageAnime() {
   let tl = gsap.timeline();
 
-  tl.to(mainContainerText1, {
-    display: 'none',
-    opacity: 0,
-    duration: 0.5,
+  tl.from('#secondPageContainer h1', {
+    transform: 'translateX(-300%)',
     scrollTrigger: {
       scroller: 'body',
-      trigger: mainContainerText1,
-      start: 'top 0%',
-      end: 'top 150%',
+      trigger: '#landingPage #firstPage',
       scrub: 2,
     }
   })
 
-  function page2() {
-    let tl = gsap.timeline();
-
-    tl.from(mainPage2Text, {
-      transform: 'translateX(-300%)',
-      scrollTrigger: {
-        scroller: 'body',
-        trigger: mainPage2,
-        start: 'top 0%',
-        end: 'top 0%',
-        pin: true,
-        scrub: 2,
-      }
-    })
-
-    tl.from(mainPage2Text2, {
-      transform: 'translateX(300%)',
-      scrollTrigger: {
-        scroller: 'body',
-        trigger: mainPage2,
-        start: 'top 0%',
-        end: 'top 0%',
-        pin: true,
-        scrub: 2,
-      }
-    })
-
-    tl.to(mainPage2Text, {
-      display: 'none',
-      opacity: 0,
-      duration: 0.5,
-      scrollTrigger: {
-        scroller: 'body',
-        trigger: '#secondPage',
-        start: 'top -10%',
-        end: 'top 70%',
-        scrub: 2
-      }
-    })
-
-    tl.to(mainPage2Text2, {
-      display: 'none',
-      opacity: 0,
-      duration: 0.5,
-      scrollTrigger: {
-        scroller: 'body',
-        trigger: '#secondPage',
-        start: 'top -10%',
-        end: 'top 70%',
-        scrub: 2
-      }
-    })
-
-    tl.to('main', {
-      display: 'none',
-      opacity: 0,
-      duration: 0.5,
-      scrollTrigger: {
-        scroller: 'body',
-        trigger: '#thirdPage',
-        start: 'top 0%',
-        end: 'top 150%',
-        scrub: 2,
-      }
-    })
-  }
-
-  page2();
+  tl.from('#secondPageContainer h2', {
+    transform: 'translateX(300%)',
+    scrollTrigger: {
+      scroller: 'body',
+      trigger: '#landingPage #firstPage',
+      scrub: 2,
+    }
+  })
 }
 
-mainTextGsap();
+secondPageAnime();
+
 
 function thirdPage() {
-  const pageh1 = document.querySelector('#thirdPageContainer h1');
-  const pageh2 = document.querySelector('#thirdPageContainer h2');
-
   let tl = gsap.timeline();
 
-  tl.from(pageh1, {
+  tl.from('#leftTop h3 ', {
     x: -1500,
     opacity: 0,
     scrollTrigger: {
       scroller: 'body',
-      trigger: '#thirdPageContainer #thirdRight',
+      trigger: '#aboutContainer #aboutRight',
       start: 'top 80%',
       end: 'top 50%',
-      scrub: 2,
+      scrub: 2
     }
   })
 
-  tl.from(pageh2, {
+  tl.from('#leftTop h2', {
     x: -1000,
     opacity: 0,
     scrollTrigger: {
       scroller: 'body',
-      trigger: '#thirdPageContainer #thirdRight',
+      trigger: '#aboutContainer #aboutRight',
       start: 'top 80%',
       end: 'top 50%',
       scrub: 4,
     }
   })
 
-  tl.from('#thirdRight', {
+  tl.from('#aboutContainer #aboutRight', {
     x: 1000,
     opacity: 0,
     scrollTrigger: {
       scroller: 'body',
-      trigger: '#thirdPageContainer #thirdRight',
+      trigger: '#aboutContainer #aboutRight',
       start: 'top 80%',
       end: 'top 50%',
       scrub: 4,
